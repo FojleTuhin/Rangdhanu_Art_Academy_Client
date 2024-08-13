57d243a2fcf409dc26e89870b737dd2e95b71a7c
fojletuhin <fojletuhin@gmail.com>
2024-08-14 01:24:40 +0600

    add react responsive carousel

--- src/home/Banner.jsx
+++ src/home/Banner.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import img1 from '../assets/1.jpg'
+import img2 from '../assets/3.jpg'
+import img3 from '../assets/4.jpg'
+
 
 const Banner = () => {
     return (
         <div>
-            
+            <Carousel className="text-center">
+                <div>
+                    <img src={img1} />
+
+                </div>
+                <div>
+                    <img src={img3} />
+
+                </div>
+                <div>
+                    <img src={img2} />
+
+                </div>
+                <div>
+                    <img src={img1} />
+
+                </div>
+                <div>
+                    <img src={img1} />
+
+                </div>
+                <div>
+                    <img src={img1} />
+
+                </div>
+
+            </Carousel>
+
         </div>
     );
 };
