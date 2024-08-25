e9359c3011e9a15800c99a0265eb7cd049b2237b
fojletuhin <fojletuhin@gmail.com>
2024-08-26 00:45:48 +0600

    polish ui

--- src/shared/Login.jsx
+++ src/shared/Login.jsx
@@ -10,18 +10,18 @@ const Login = () => {
             </div>
 
             <div className="flex flex-col md:items-end items-center">
-                <div className="text-white md:w-[40%]">
+                <div className="text-white md:w-[40%] w-[90%] p-5 rounded-lg bg-black bg-opacity-60 md:bg-none md:bg-opacity-0">
                     <p className="mt-4 mb-1 px-4">Name</p>
-                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="text" name="" id="" required placeholder="Fojle Tuhin" />
+                    <input className="md:w-[70%] w-full px-4 py-1  bg-black bg-opacity-30 rounded-lg" type="text" name="" id="" required placeholder="Fojle Tuhin" />
                     <p className="mt-4 mb-1 px-4">E-mail</p>
-                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="email" name="" id="" required placeholder="fojletuhin@gmail.com" />
+                    <input className="md:w-[70%] w-full px-4 py-1  bg-black bg-opacity-30 rounded-lg" type="email" name="" id="" required placeholder="fojletuhin@gmail.com" />
                     <p className="mt-4 mb-1 px-4">Photo URL</p>
-                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="text" name="" required placeholder="Enter photo Url" />
+                    <input className="md:w-[70%] w-full px-4 py-1  bg-black bg-opacity-30 rounded-lg" type="text" name="" required placeholder="Enter photo Url" />
                     <p className="mt-4 mb-1 px-4">Password</p>
-                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="password" name="" id="" required placeholder="**********" />
+                    <input className="md:w-[70%] w-full px-4 py-1  bg-black bg-opacity-30 rounded-lg" type="password" name="" id="" required placeholder="**********" />
 
                     <br />
-                    <button className="md:w-[70%] px-4 py-2 mt-5 bg-green-500 rounded-lg text-black font-bold" type="submit">Sign Up</button>
+                    <button className="md:w-[70%] w-full px-4 py-2 mt-5 bg-green-500 rounded-lg text-white font-bold" type="submit">Sign Up</button>
 
                 </div>
 
