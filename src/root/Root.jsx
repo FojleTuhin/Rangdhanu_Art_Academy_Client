7f1e3b2651eb5ae7956100dde7575380283835a8
fojletuhin <fojletuhin@gmail.com>
2024-08-26 00:31:03 +0600

    create sign in page

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
 
 const Root = () => {
+    const location = useLocation()
+    const isLogin = location.pathname.includes('login') || location.pathname.includes('signUp')
     return (
         <div className="">
-            <Navbar />
+            {isLogin || <Navbar />}
             <Outlet></Outlet>
-            <Footer />
+            {isLogin || <Footer />}
         </div>
     );
 };
--- src/shared/Login.jsx
+++ src/shared/Login.jsx
@@ -1,10 +1,31 @@
+import { Link } from "react-router-dom";
 
 
-// import loginPicture from '../assets/1.jpg'
 const Login = () => {
     return (
-        <div className="bg-[url('../assets/1.jpg')]">
-            I am from Login
+        <div className="flex flex-col bg-[url('https://i.ibb.co/tC8Tg1B/1.jpg')] min-h-screen bg-cover">
+            <div className="flex gap-5 justify-end text-white p-10 font-bold">
+                <Link to='/' className="link-hover">Home</Link>
+                <Link to='/signUp' className="link-hover">Sign up</Link>
+            </div>
+
+            <div className="flex flex-col md:items-end items-center">
+                <div className="text-white md:w-[40%]">
+                    <p className="mt-4 mb-1 px-4">Name</p>
+                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="text" name="" id="" required placeholder="Fojle Tuhin" />
+                    <p className="mt-4 mb-1 px-4">E-mail</p>
+                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="email" name="" id="" required placeholder="fojletuhin@gmail.com" />
+                    <p className="mt-4 mb-1 px-4">Photo URL</p>
+                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="text" name="" required placeholder="Enter photo Url" />
+                    <p className="mt-4 mb-1 px-4">Password</p>
+                    <input className="md:w-[70%] px-4 py-1 md:bg-green-500 bg-black bg-opacity-30 rounded-lg" type="password" name="" id="" required placeholder="**********" />
+
+                    <br />
+                    <button className="md:w-[70%] px-4 py-2 mt-5 bg-green-500 rounded-lg text-black font-bold" type="submit">Sign Up</button>
+
+                </div>
+
+            </div>
         </div>
     );
 };
